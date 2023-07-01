01e7eeea0b868f6eef49a18108543ca1a8920657
E. M. Greeff <emgreeff@datadash.cloud>
2023-07-01 01:17:59 -0600

    to do: confetti

--- main.js
+++
@@ -1,5 +0,0 @@
-import { createRoot } from "npm:react-dom@18.2.0/client";
-import App from "./App.tsx";
-
-const root = createRoot(getElementById("root"));
-root.render(App);
--- main.ts
+++
@@ -1,6 +0,0 @@
-import React from "https://esm.sh/react@18.2.0";
-import { createRoot } from "react-dom";
-import App from "./app.js";
-
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
